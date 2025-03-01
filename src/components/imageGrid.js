007e748df24592173d96d38a5ab57b01bb046659
stbam <55661516+stbam@users.noreply.github.com>
2025-02-28 22:24:32 -0500

    added book page

---
+++ src/components/imageGrid.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+// Import images
+import img1 from "../assets/test_book_images/alena.jpg";
+import img2 from "../assets/test_book_images/dead_astronauts.jpg";
+import img3 from "../assets/test_book_images/Fallingman.jpg";
+import img4 from "../assets/test_book_images/leviathan.jpg";
+import img5 from "../assets/test_book_images/love_sad.jpg";
+
+// Styled component for transparent background
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: 'transparent',
+  boxShadow: 'none',
+  outline: 'none',
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+// Array of book objects with images and genres
+const books = [
+  { image: img1, genres: ["Romance", "Drama"] },
+  { image: img2, genres: ["Action", "Adventure"] },
+  { image: img3, genres: ["Fantasy", "Mystery", "Thriller"] },
+  { image: img4, genres: ["Sci-Fi", "Dystopian"] },
+  { image: img5, genres: ["Horror", "Supernatural"] },
+  { image: img1, genres: ["Comedy", "Action"] },
+  { image: img2, genres: ["Historical", "Biography"] },
+  { image: img3, genres: ["Romance", "Fantasy"] },
+  { image: img4, genres: ["Sci-Fi", "Action"] },
+  { image: img5, genres: ["Mystery", "Adventure"] },
+
+];
+
+
+export default function BasicGrid() {
+  return (
+    <Box>
+      <Grid container spacing={2} justifyContent="center">
+        {books.map((book, index) => (
+          <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+            <Item>
+              <Card sx={{ width: 300, backgroundColor: 'transparent',boxShadow: 'none',outline: 'none',}}>
+                <CardMedia component="img" height="300" image={book.image} alt={`Book ${index + 1}`} sx={{objectFit:'contain',width:'100%'}} />
+                <CardContent>
+                  <Box sx={{display:'flex',gap:'15px', marginLeft:'33px'}} >
+                  {/* Display genres */}
+                  {book.genres.map((genre, i) => (
+                    <Typography key={i} variant="body2" color="text.secondary" 
+                        sx={{display:'flex',flexDirection:'column',
+                              color:'#f5f5f5',backgroundColor:'#3e3e3e',
+                              borderRadius:'5px',
+                             
+                              }}>
+                      {genre}
+                    </Typography>
+                  ))}
+                  </Box>
+                </CardContent>
+              </Card>
+            </Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,11 @@ import React from "react";
 import Carousel from "../components/carousel";
 import Trending from "./Trending";
 import Card from '../components/Card'
+import { padding } from "@mui/system";
+import { Link } from 'react-router-dom';
+
+import arrow from "../assets/images/Arrow right-circle.png";
+
 function Home() {
     return (
       <div className="home-box">
@@ -13,13 +18,20 @@ function Home() {
         <div className="foryou-box">
           {/* Carousel code from carousel.js */}
           <Carousel/>
-          <div className="library-cards">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            
+          <div className="library-cards-subtitle">
+            <div className="subtitle">
+              <h1 style={{color:'white'}}>Books</h1>
+              <h2 style={{margin:'24px',cursor:'pointer',color:'gray'}}><Link to="/books"id="grey-title" >See more <img src={arrow} style={{width:'15px',margin:'0px',cursor:'pointer'}} ></img></Link> </h2> {/* in line style meant to align the Home and See More */}
+            </div>
+            <div className="library-cards">
+              <Card></Card>
+              <Card></Card>
+              <Card></Card>
+              <Card></Card>
+              
+            </div>
           </div>
+         
           
           
         </div>
